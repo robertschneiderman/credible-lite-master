af4f6b38574f94ade3e036a5c23588a67525c5b9
Robert Schneiderman <robert.a.schneiderman@gmail.com>
2017-01-24 21:15:11 -0800

    reactable table implemented for sorting

--- frontend/results/components/index.js
+++ frontend/results/components/index.js
@@ -1,7 +1,19 @@
 import React, {Component} from 'react';
 import {connect} from 'react-redux';
+import {Table} from 'reactable';
 import * as offersActions from '../../offers/actions';
 import * as selectedOffersActions from '../../selected_offers/actions';
+
+const titleCase = (str) => {
+   var splitStr = str.toLowerCase().split(' ');
+   for (let i = 0; i < splitStr.length; i++) {
+       // You do not need to check if i is larger than splitStr length, as your for does that for you
+       // Assign it back to the array
+       splitStr[i] = splitStr[i].charAt(0).toUpperCase() + splitStr[i].substring(1);     
+   }
+   // Directly return the joined string
+   return splitStr.join(' '); 
+};
 
 class Results extends Component {
     constructor(props) {
@@ -12,6 +24,7 @@ class Results extends Component {
 
         this.selectRow = this.selectRow.bind(this);
         this.handleSubmit = this.handleSubmit.bind(this);
+        this.beautifyData = this.beautifyData.bind(this);
     }
 
     componentWillMount() {
@@ -49,21 +62,43 @@ class Results extends Component {
             );
         });
     }
+
+    beautifyData(data) {
+                // debugger;
+        if (data.length > 0) {
+            let rows = [];
+            let keyOrder = ['name', 'term', 'monthly_payment', 'total_interest', 'total_cost', 'apr'];
+
+            data.forEach((row, i) => {
+                rows.push({});
+                keyOrder.forEach(key => {
+                    rows[i][titleCase(key.split('_').join(" "))] = row[key];
+                });
+            });
+
+            return rows;
+        }
+    }
+    
     render() {
+                // <table className="results">
+                //     <tr>
+                //         <th>Lender's Name</th>              
+                //         <th>Product Term</th>              
+                //         <th>APR</th>              
+                //         <th>Monthly Payment</th>              
+                //         <th>Total Cost</th>              
+                //         <th>Total Interest</th>
+                //     </tr>
+                //     {this.renderOffers()}
+                // </table>
+
+        let data = this.beautifyData(this.props.offers);
+
         return(
             <div className="results-page">
-                <table className="results">
-                    <tr>
-                        <th>Lender's Name</th>              
-                        <th>Product Term</th>              
-                        <th>APR</th>              
-                        <th>Monthly Payment</th>              
-                        <th>Total Cost</th>              
-                        <th>Total Interest</th>
-                    </tr>
-                    {this.renderOffers()}
-                </table>
-                <button onClick={this.handleSubmit}className="btn-results">Compare Offers</button>
+            <Table className="results" data={data} sortable={true} />            
+            <button onClick={this.handleSubmit}className="btn-results">Compare Offers</button>
             </div>
         );
     }
