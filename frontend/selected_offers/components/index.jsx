bbcb4e60baa7992ae9de59f4d3e1a0c0171be33d
Robert Schneiderman <robert.a.schneiderman@gmail.com>
2017-01-24 18:39:35 -0800

    implementation of chart.js for selected offers

--- frontend/selected_offers/components/index.jsx
+++ frontend/selected_offers/components/index.jsx
@@ -2,7 +2,7 @@ import React, {Component} from 'react';
 import {connect} from 'react-redux';
 import * as actions from '../actions';
 
-// import {BarChart} from 'react-chartjs';
+import {Bar} from 'react-chartjs';
 
 class SelectedOffers extends Component {
     constructor(props) {
@@ -10,22 +10,46 @@ class SelectedOffers extends Component {
     }
 
     componentDidMount() {
-        let selectedOffers = this.props.selectedOffers ? this.props.selectedOffers : JSON.parse(localStorage.getItem('selectedOffers'));
+        let selectedOffers = this.props.selectedOffers.length > 0 ? this.props.selectedOffers : JSON.parse(localStorage.getItem('selectedOffers'));
         localStorage.setItem('selectedOffers', JSON.stringify(selectedOffers));
     }
 
     renderSelectedOffers() {
         let selectedOffers = (this.props.selectedOffers.length === 0) ? JSON.parse(localStorage.getItem('selectedOffers')) : this.props.selectedOffers;
-        return selectedOffers.map(offer => {
-            return <li className="selected-offer" >{offer.monthly_payment}</li>;
+        let data = {
+            labels: [],
+            datasets: [{
+                data: [100, 100],
+                backgroundColor: [
+                    'rgba(255, 99, 132, 0.2)',
+                    'rgba(54, 162, 235, 0.2)'
+                ],
+                fillColor: [
+                    'rgba(255,99,132,1)',
+                    'rgba(54, 162, 235, 1)',
+                ]               
+            }]
+        };
+        selectedOffers.forEach((offer, i) => {
+            let key = "monthly_payment";
+            // Object.keys(offer).forEach(key => {
+                // if (!data.datasets.data) data.datasets.data = [];
+                // if (!data.labels) data.labels = [];
+                // debugger;
+                // data.datasets[0].data.push(offer["monthly_payment"]);
+                data.labels.push(`offer ${i+1}`);
+            // });
         });
+        // return selectedOffers.map(offer => {
+            return <Bar data={data} width="600" height="250" className="selected-offer" />;
+        // });
     }
 
     render() {
         return(
-            <table className="selected-offers">
+            <div className="selected-offers">
                 {this.renderSelectedOffers()}
-            </table>
+            </div>
         );
     }
 }
